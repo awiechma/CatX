c9fc366e60d6dd39a2d0177fc167c285d6b60099
anienabe <anke.nienaber@uni-muenster.de>
2024-11-09 13:50:44 +0100

    added links to all subpages

---
+++ react-app-catx/src/Add.jsx
@@ -0,0 +1,18 @@
+// import all the components used in the app
+import React from "react";
+import './Add.css';
+import Button from "./components/Button";
+import Navbar from "./components/Navbar";
+import SearchBar from "./components/SearchBar";
+
+
+const Add = () => {
+    return (
+        <div>
+            <h1>Add Page</h1>
+            <p>This is the Add page content.</p>
+        </div>
+    );
+};
+
+export default Add;
--- react-app-catx/src/Join.jsx
+++ react-app-catx/src/Join.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import App from './App';
 import View from './View';
+import Add from './Add';
+import Account from './Account';
+import Help from './Help';
 
 const Join = () => {
     return (
@@ -11,6 +14,9 @@ const Join = () => {
                 <Routes>
                     <Route path="/" element={<App/>} />
                     <Route path="/view" element={<View/>} />
+                    <Route path="/add" element={<Add/>} />
+                    <Route path="/account" element={<Account/>} />
+                    <Route path="/help" element={<Help/>} />
                 </Routes>
         </Router>
     );
