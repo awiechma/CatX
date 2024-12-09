db3cd62fb56c498c9da0268e3c2f5bb383f3386b
Andreas Rademaker <arademak@uni-muenster.de>
2024-12-09 12:16:58 +0100

    add user role, add user data request

--- backend/node/server.js
+++ backend/node/server.js
@@ -106,6 +106,32 @@ app.post('/api/validatetoken', passport.authenticate('jwt', { session: false }),
   }
 })
 
+app.post('/api/user/:username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  const username = req.params.username;
+  if (!username) {
+    return res.status(400).json({ message: 'Username is required.' });
+  }
+  if (req.user.role < 2 && username != req.user.username) {
+    return res.status(403).json({ message: 'Forbidden.' });
+  }
+  const query = {
+    text: `
+        SELECT username, full_name, email, role
+        FROM users
+        WHERE username = $1
+      `,
+    values: [username],
+  }
+
+  db.query(query)
+    .then(({ rows: user }) => res.status(200).json((user.length > 0) ? user[0] : {}))
+    .catch(error => {
+      console.error('Error during user export', error);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+});
+
+
 /**
  * Endpoint to retrieve providers from the database
  * Accepts optional limit and offset parameters in the request body for pagination
