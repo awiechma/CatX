23fe4ceb1aabf498452bb1616de8f8a6402c422b
Amon Wiechmann <awiechma@uni-muenster.de>
2024-12-17 19:26:00 +0100

    ahsdflk

--- react-app-catx/src/Account.jsx
+++ react-app-catx/src/Account.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import './Account.css';
-import Button from "./components/Button";
 import Navbar from "./components/Navbar";
-import SearchBar from "./components/SearchBar";
-import LogInSignIn from "./components/LogInSignIn";
 import Footer from "./components/Footer";
 import AccountHandler from "./components/AccountHandler";
 
@@ -11,11 +8,9 @@ import AccountHandler from "./components/AccountHandler";
 const Account = () => {
     return (
         <div>
-            <Navbar />
-            <SearchBar />
+            <Navbar/>
             <AccountHandler/>
-            <Footer />
-
+            <Footer/>
         </div>
     );
 };
