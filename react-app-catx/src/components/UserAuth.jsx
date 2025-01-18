717abb880543f349e0dc294605d79e78b613438a
Amon Wiechmann <ajwiechmann@web.de>
2025-01-18 12:08:40 +0100

    Extended user auth (#48)
    * Implemented: Extern User Auth
    
    * Fully working user Auth
    
    ---------
    
    Co-authored-by: Amon Wiechmann <awiechma@uni-muenster.de>

--- react-app-catx/src/components/UserAuth.jsx
+++ react-app-catx/src/components/UserAuth.jsx
@@ -6,9 +6,7 @@ const useAuth = () => {
     useEffect(() => {
         const checkLoginStatus = async () => {
             const token = localStorage.getItem("catx-user-session-token");
-            console.log("Token aus localStorage:", token); // Token anzeigen
             const username = localStorage.getItem("catx-user-session-username");
-            console.log("Username aus localStorage:", username); // Username anzeigen
             if (token && username) {
                 try {
                     const response = await fetch("http://localhost:3000/api/validatetoken", {
