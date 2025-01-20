16375d7b9d025321e8cfb86bc9ce3f4f08a6460d
Amon Wiechmann <ajwiechmann@web.de>
2025-01-20 12:59:06 +0100

    Detail view (#51)
    * Implemented: Detailed View with Ids
    
    * Fix: Itemlist
    
    ---------
    
    Co-authored-by: Amon Wiechmann <awiechma@uni-muenster.de>

--- react-app-catx/src/components/Navbar.jsx
+++ react-app-catx/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
         <a className="navbar-brand" onClick={handleLogoClick} href="#" style={{ fontSize: '1.5rem', marginRight: '1rem', marginLeft: '1rem' }}>
-          <img src="./CatX_Logo.png" alt="Logo" width="35" height="35" className="d-inline-block align-text-top" style={{ fontSize: '1.5rem', marginRight: '1rem' }} />
+          <img src="../../CatX_Logo.png" alt="Logo" width="35" height="35" className="d-inline-block align-text-top" style={{ fontSize: '1.5rem', marginRight: '1rem' }} />
           Cat <p style={{ fontFamily: 'Times New Roman' }}> &chi;</p>
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
